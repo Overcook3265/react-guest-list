e3c8692c97049fb24324261214b0aac5cc37baeb
Overcook3265 <151878132+Overcook3265@users.noreply.github.com>
2024-01-25 14:55:35 +0100

    Add submit function with Enter key and console log

--- src/App.js
+++ src/App.js
@@ -4,39 +4,41 @@ import styles from './App.module.scss';
 
 export default function App() {
   const [firstName, setFirstName] = useState('');
-  const [familyName, setFamilyName] = useState('');
-  const initialGuests = [{ name: '', family: '' }];
+  const [lastName, setLastName] = useState('');
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    console.log(`Submitted ${firstName} ${lastName} `);
+  };
+  const initialGuests = [{ name: '', last: '' }];
   return (
     <div className={styles.app}>
       <div className="Wrapper">
         <header className={styles.header}>
           <h1>Who's Coming?</h1>
         </header>
-        <body>
-          <section className={styles.inputfields}>
-            <label>
-              Vorname
-              <input
-                placeholder={'Vorname'}
-                onChange={(event) => {
-                  setFirstName(event.currentTarget.value);
-                }}
-              />
-            </label>
-            <label>
-              Nachname
-              <input
-                placeholder={'Nachname'}
-                onChange={(event) => {
-                  setFamilyName(event.currentTarget.value);
-                }}
-              />
-            </label>
-          </section>
-          <div>
-            {firstName} {familyName}
-          </div>
-        </body>
+        <form className={styles.inputfields} onSubmit={handleSubmit}>
+          <label>
+            Vorname
+            <input
+              placeholder={'Vorname'}
+              onChange={(event) => {
+                setFirstName(event.target.value);
+              }}
+            />
+          </label>
+          <label>
+            Nachname
+            <input
+              placeholder={'Nachname'}
+              onChange={(event) => {
+                setLastName(event.target.value);
+              }}
+            />
+          </label>
+          <button type="submit">Submit</button>
+        </form>
+        <div>{/* {firstName} {lastName} */}</div>
       </div>
     </div>
   );
